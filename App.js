8b26eb06fe6aaee098e3054c558e82f72012b497
swing-park <ongsim123@gmail.com>
2023-10-06 18:09:05 +0900

    :sparkles: Feat : weather app

--- App.js
+++ App.js
@@ -1,24 +1,90 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import * as Location from "expo-location";
+import { useEffect, useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  Dimensions,
+  ActivityIndicator,
+} from "react-native";
+
+const { width: SCREEN_WIDTH } = Dimensions.get("window");
+const API_KEY = "98fd8fa0e9dabdd59e80a519e012693f";
 
 export default function App() {
+  const [city, setCity] = useState("Loading...");
+  const [weatherData, setWeatherData] = useState();
+  const [permission, setPermission] = useState(true);
+
+  const getWeather = async () => {
+    const { granted } = await Location.requestForegroundPermissionsAsync();
+    if (!granted) {
+      setPermission(false);
+    }
+
+    const {
+      coords: { latitude, longitude },
+    } = await Location.getCurrentPositionAsync({ accuracy: 5 });
+
+    const location = await Location.reverseGeocodeAsync(
+      { latitude, longitude },
+      { useGoogleMaps: false }
+    );
+
+    setCity(location[0].city);
+
+    const res = await fetch(
+      `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`
+    );
+
+    const json = await res.json();
+
+    setWeatherData(json.list);
+    console.log(json.list);
+  };
+
+  useEffect(() => {
+    getWeather();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Hello World !</Text>
-      <StatusBar style="auto" />
+      <View style={styles.city}>
+        {}
+        <Text style={styles.cityName}>{city}</Text>
+      </View>
+      <ScrollView
+        pagingEnabled
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={styles.weather}
+      >
+        {weatherData?.map((v) => (
+          <View style={styles.day} key={v.dt_txt}>
+            <Text style={styles.temp}>{v.dt_txt}</Text>
+            <Text style={styles.desc}>{JSON.stringify(v)}</Text>
+          </View>
+        ))}
+      </ScrollView>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  container: { flex: 1, backgroundColor: "orange" },
+  city: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
     justifyContent: "center",
+    alignItems: "center",
   },
-  text: {
-    color: "red",
-    fontSize: 28,
+  cityName: { fontSize: 68, fontWeight: 600 },
+  weather: {},
+  day: {
+    width: SCREEN_WIDTH,
+    alignItems: "center",
   },
+  temp: { fontSize: 60, marginTop: 50 },
+  desc: { fontSize: 14 },
+  tinyText: { fontSize: 20 },
 });
